4a1be5e8f5ab03b9655c706f3db3c0eae80dd37c
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-10-21 18:05:25 +0200

    modal modification

---
+++ main.js
@@ -0,0 +1,66 @@
+var backProject = document.getElementById("openmodal");
+var modal = document.querySelector(".modal-container");
+
+backProject.addEventListener("click", event => {
+    modal.style.display = "flex";
+});
+
+
+
+// const openModal = document.getElementById("openmodal");
+// const modalContainer = document.getElementById("modalcontainer");
+
+// openModal.addEventListener('click' () => {
+//     modalContainer.classList.add('show');
+// });
+
+// var modal = document.getElementById("probica");
+// var nonModal = document.getAnimations("non");
+// var openBtn = document.getElementById("openmodal");
+// var closeBtn = document.getElementById("closemodal");
+
+
+
+// openBtn.onclick = function() {
+//     nonModal.style.display = "none";
+//     modal.style.display = "block";
+    
+// }
+
+// closeBtn.onclick = function() {
+//     console.log(123);   // modal.style.display = "none";
+// }
+
+
+
+// openBtn.addEventListener('click', openModal);
+// closeBtn.addEventListener('click', closeModal);
+
+// function openModal() {
+//     modal.style.display = 'block';
+// }
+
+// function closeModal() {
+//     modal.style.display = 'none';
+// }
+
+
+// window.onclick = function(event) {
+//     if (event.target == modalContainer) {
+//       modalContainer.style.display = "none";
+//     }
+//   }
+
+
+
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "350px";
+//     document.getElementsByTagName("body")[0].style.opacity = "0.5";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementsByTagName("body")[0].style.opacity = "1";
+//   }
+
+
